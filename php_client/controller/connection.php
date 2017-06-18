ba8907885c856f4dc93f50d96b39112eca75dc1a
root <root@conges.fcnet>
2017-06-18 23:38:12 +0200

    Add model

--- php_client/controller/connection.php
+++ php_client/controller/connection.php
@@ -1,10 +1,11 @@
 <?php
-include('controller/db_parameters.php');
+include('controller/db_parameters_prod.php');
 	try
 	{
 		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 		$pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES utf8";
-		$bdd = new PDO($DB_TYPE.':host='.$DB_HOST.';dbname='.$DB_NAME, $DB_USER, $DB_PASSWORD, $pdo_options);
+		$con =$DB_TYPE.':host='.$DB_HOST.';dbname='.$DB_NAME;
+		$bdd = new PDO($con, $DB_USER, $DB_PASSWORD, $pdo_options);
 	}
 	catch (Exception $e)
 	{
