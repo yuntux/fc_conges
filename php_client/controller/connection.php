da5e7da064dc777c77a5c46804945f631a8ab642
root <root@conges.fcnet>
2017-06-18 23:14:32 +0200

    GIt ignore

--- php_client/controller/connection.php
+++ php_client/controller/connection.php
@@ -1,11 +1,10 @@
 <?php
-$GUERANDE="n2sGZ93z";
-header('Content-Type: text/html; charset=utf-8');
+include('controller/db_parameters.php');
 	try
 	{
 		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 		$pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES utf8";
-		$bdd = new PDO('mysql:host=localhost;dbname=fc_conges', 'root', 'tb3cLe584eSRL3P9', $pdo_options);
+		$bdd = new PDO($DB_TYPE.':host='.$DB_HOST.';dbname='.$DB_NAME, $DB_USER, $DB_PASSWORD, $pdo_options);
 	}
 	catch (Exception $e)
 	{
