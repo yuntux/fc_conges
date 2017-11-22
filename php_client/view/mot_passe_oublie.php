983300495c6bca8b9952649e52c38e0f9fa18830
root <root@conges.fcnet>
2017-11-22 23:33:47 +0100

    Add files

---
+++ php_client/view/mot_passe_oublie.php
@@ -0,0 +1,19 @@
+
+		<form action="?action=mot_passe_oublie" method="post">
+			<table style="position:absolute;left:55%;top:30%;">
+				<thead>
+					<tr>
+						<td colspan=2 style ="color : grey ;font-size : 18px;">Connexion</td>
+					</tr>
+				</thead>
+				<tbody style="position: fixed;border-radius : 5px ;">
+					<tr>
+						<td>E-mail : </td>
+						<td><input type="text" name="login" /></td>
+					</tr>
+					<tr>
+						<td><input type="submit" name="mdp_oublie" value="Mot de passe oublié" style="margin-left: 0em;"/></td>
+					</tr>
+				</tbody>
+			</table>
+		</form>			
